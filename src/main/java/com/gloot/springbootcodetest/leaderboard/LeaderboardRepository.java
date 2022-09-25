f249d559137c9d23a7ef32399aed3b60b7ea4d1c
fpapi <erkito87@gmail.com>
2022-09-25 18:39:19 +0200

    Leaderboard service sorting refactor using query
    Find default leaderboard using query
    Implement sorting with a single java stream

--- src/main/java/com/gloot/springbootcodetest/leaderboard/LeaderboardRepository.java
+++ src/main/java/com/gloot/springbootcodetest/leaderboard/LeaderboardRepository.java
@@ -1,8 +1,13 @@
 package com.gloot.springbootcodetest.leaderboard;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LeaderboardRepository extends JpaRepository<LeaderboardEntryEntity, UUID> {}
+public interface LeaderboardRepository extends JpaRepository<LeaderboardEntryEntity, UUID> {
+	
+	List<LeaderboardEntryEntity> findByLeaderboard_Name(String boardName);
+	
+}
